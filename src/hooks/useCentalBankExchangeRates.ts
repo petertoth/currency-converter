ccda0b044ffd504faa7bc9dbcd819a7ef8a7a133
Peter Toth <peter@petertoth.me>
2025-03-09 18:26:18 +0100

    use vitePluginVercelApi for exchange rates api route

--- src/hooks/useCentalBankExchangeRates.ts
+++ src/hooks/useCentalBankExchangeRates.ts
@@ -12,13 +12,9 @@ function parseCentralBankExchangeRates(data: string): CurrencyExchangeRate[] {
   // split by new line and filter out empty lines
   const lines = data.split('\n').filter(Boolean)
 
-  console.log({ lines })
-
   // Skip the first two lines (header and date)
   const rates = lines.slice(2).map(line => {
     const [country, currency, amount, code, rate] = line.split('|')
-
-    // console.log({ country, currency, amount, code, rate })
 
     return {
       country,
@@ -29,13 +25,11 @@ function parseCentralBankExchangeRates(data: string): CurrencyExchangeRate[] {
     } satisfies CurrencyExchangeRate
   })
 
-  console.log({ rates })
-
   return rates;
 }
 
 async function fetchCentralBankExchangeRates() {
-  const response = await fetch('/currency-exchange-rates')
+  const response = await fetch('/api/currency-exchange-rates')
   const data = await response.text()
 
   const parsedData = parseCentralBankExchangeRates(data)
